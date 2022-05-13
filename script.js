ce28ccd4e4bcf77bd913bb682c7be96629a532f2
Kaushal Sharma <skazi019@gmail.com>
2022-05-13 11:58:59 +0530

    Only box remaining

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 let acc = document.getElementsByClassName('dropdown');
+var i;
 
-for (let i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", toggleContent);
-}
-
-function toggleContent() {
-    let panel = this.nextSibling;
-    panel = panel.nextSibling;
-    console.log(panel);
-    if (panel.style.display === "block") {
-        panel.style.display = "none";
-    } else {
-        panel.style.display = "block";
-    }
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.previousElementSibling.classList.remove("text-bold")
+            panel.style.maxHeight = null;
+            panel.previousElementSibling.children[0].children[0].classList.remove("flip")
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+            panel.previousElementSibling.classList.add("text-bold")
+            panel.previousElementSibling.children[0].children[0].classList.add("flip")
+        }
+    });
 }
